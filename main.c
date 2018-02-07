44d50825b889ca7fe9e50ed8c0807337421f9efc
Anton Sapunov <fort.sav.28@gmail.com>
2018-02-07 18:05:50 +0700

    after hamming

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <viterby.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main()
 {
-    uint32_t data = 0xB;
+    uint32_t data = 0xD7677;
     uint32_t *coded = &data;
     size_t lenCoded = 20;
-    uint32_t *decoded;
-    size_t lenDecoded = 4;
+    size_t lenDecoded = lenCoded / N - 6;
+    uint32_t *decoded = (uint32_t *)malloc(lenDecoded);	// Формула представлена в пункте 7.3 G.9903
 
+
+    //printf("Bytes: %d\n", lenDecoded / 8 + 1);
     getDecode(coded, lenCoded, decoded, lenDecoded);
+
+    free(decoded);
     return 0;
 }
