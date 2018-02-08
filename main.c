a6f314ef0c8cd4f1bac0dfffecbad04d22b388cf
Anton Sapunov <fort.sav.28@gmail.com>
2018-02-08 12:55:35 +0700

    test2

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <viterby.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    uint32_t data = 0xD7677;
+    uint32_t *coded = &data;
+    size_t lenCoded = 20;
+    size_t lenDecoded = lenCoded / N - 6;
+    uint32_t *decoded = (uint32_t *)malloc(lenDecoded);	// Формула представлена в пункте 7.3 G.9903
+
+
+    //printf("Bytes: %d\n", lenDecoded / 8 + 1);
+    getDecode(coded, lenCoded, decoded, lenDecoded);
+
+    free(decoded);
     return 0;
 }
